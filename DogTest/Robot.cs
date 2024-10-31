caf1e17ebf8931860bd8a71c2fd5d7261ae51635
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-10-31 15:26:37 +0900

    SR5

---
+++ DogTest/Robot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Robot
+    {
+        //フィールド
+        protected string name = "";
+        protected bool powerStatus = false;
+
+        //メソッド
+        //電源をonにする
+        public void PowerOn()
+        {
+            powerStatus = true;
+            Console.WriteLine("{0}は、起動した!!", name);
+        }
+
+        //電源をOFFにする
+        public void PowerOFF()
+        {
+            powerStatus = false;
+            Console.WriteLine("{0}は、停止した", name);
+        }
+        //名前を取得する
+        public string GetName()
+        {
+            return name;
+        }
+
+        //電源On状態を取得する
+        public bool GetPowerStatus()
+        {
+            return powerStatus;
+        }
+    }
+}
---
+++ DogTest/TankRobot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class TankRobot : Robot
+    {
+        public TankRobot(string name)
+        {
+            this.name = name;
+        }
+
+        //キャノン砲を撃つ
+        public void ShootCannon()
+        {
+            if(powerStatus)
+            {
+                Console.WriteLine("{0}は、キャノン砲を撃った", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}は、電源Offなのでキャノン砲は使えません..", name);
+            }
+        }
+
+
+    }
+}
